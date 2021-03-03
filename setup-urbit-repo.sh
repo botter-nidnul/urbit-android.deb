303d04ca0feab92320cc032f6c6fe3fb86c839ad
botter <botter-nidnul@protonmail.com>
2021-03-03 06:55:21 -0600

    Create setup-urbit-repo.sh

---
+++ setup-urbit-repo.sh
@@ -0,0 +1,17 @@
+#!/data/data/com.termux/files/usr/bin/sh
+# Get some needed tools. coreutils for mkdir command, gnugp for the signing key, and apt-transport-https to actually connect to the repo
+apt-get update
+apt-get --assume-yes upgrade
+apt-get --assume-yes install coreutils gnupg
+# Make the sources.list.d directory
+mkdir -p $PREFIX/etc/apt/sources.list.d
+# Write the needed source file
+echo "deb https://botter-nidnul.github.io/urbit-android.deb/apt buster custom" > $PREFIX/etc/apt/sources.list.d/pointless.list
+# Add signing key from https://its-pointless.github.io/pointless.gpg
+if [ -n $(command -v curl) ]; then
+curl -sL https://botter-nidnul.github.io/urbit-android.deb/public.gpg | apt-key add -
+elif [ -n $(command -v wget) ]; then
+wget -qO - https://botter-nidnul.github.io/urbit-android.deb/public.gpg | apt-key add -
+fi
+# Update apt
+apt update
